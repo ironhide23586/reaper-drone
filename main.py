500f30e7f3bc28a648dc951e8bada3c05cbdbaaa
Souham Biswas <souham.biswas@outlook.com>
2023-05-31 19:12:22 +0530

    added main file

---
+++ main.py
@@ -0,0 +1,17 @@
+"""  _
+    |_|_
+   _  | |
+ _|_|_|_|_
+|_|_|_|_|_|_
+  |_|_|_|_|_|
+    | | |_|
+    |_|_
+      |_|
+
+Author: Souham Biswas
+Website: https://www.linkedin.com/in/souham/
+"""
+
+
+if __name__ == '__main__':
+    print('Reaper Says Hi! :D')
