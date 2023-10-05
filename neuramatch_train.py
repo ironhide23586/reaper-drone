fb356d13a2d221b08612d80a5e830e9200320435
Souham Biswas <souham.biswas@outlook.com>
2023-10-05 17:37:18 +0530

    accidentally initial checkpointing

--- neuramatch_train.py
+++ neuramatch_train.py
@@ -117,8 +117,8 @@ if __name__ == '__main__':
                    'train_loss': [],
                    'num_samples': []}
 
-    # checkpoint_model(nmatch, None, device, data_loader_val, ima, imb, model_dir, loss_fn, ei, bi, sess_id, log_fname,
-    #                  val_df_dict, SIDE, viz_dir)
+    checkpoint_model(nmatch, None, device, data_loader_val, ima, imb, model_dir, loss_fn, ei, bi, sess_id, log_fname,
+                     val_df_dict, SIDE, viz_dir)
 
     for ei in range(NUM_EPOCHS):
         nmatch.train()
